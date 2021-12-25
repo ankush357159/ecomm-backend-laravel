b898d8042230a612a5e592d00cde953a11c4987a
Ankush <ankush357159@gmail.com>
2021-12-26 00:03:02 +0530

    Product Details

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    function addProduct (Request $req) {
+    function addProduct(Request $req)
+    {
         $product = new Product();
         $product->name = $req->input('name');
         $product->price = $req->input('price');
@@ -18,16 +19,28 @@ class ProductController extends Controller
         return $product;
     }
 
-    function list() {
-        return Product::all(); 
+    function list()
+    {
+        return Product::all();
     }
 
-    function delete($id) {
+    function delete($id)
+    {
         $result = Product::where('id', $id)->delete();
-        if($result) {
-            return ["result"=>"Product is deleted"];
+        if ($result) {
+            return ["result" => "Product is deleted"];
         } else {
-            return ["result"=> "Product you are looking for do not exist"];
+            return ["result" => "Product you are looking for do not exist"];
+        }
+    }
+
+    function getProduct($id)
+    {
+        $result = Product::find($id);
+        if (empty($result)) {
+            return ["result" => "Product you are looking for do not exist"];
+        } else {
+            return $result;
         }
     }
 }
